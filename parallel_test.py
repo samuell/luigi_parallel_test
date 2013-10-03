760a4bbc0ca8ebd2edcd95d4ab048361f35c8c3d
Samuel Lampa <samuel.lampa@gmail.com>
2013-10-03 20:42:56 +0200

    More bugfixes

--- parallel_test.py
+++ parallel_test.py
@@ -20,17 +20,17 @@ class Merged(luigi.Task):
         return luigi.LocalTarget("output.txt")
         
     def run(self):
-        datas = []        
+        datas = []
         
         for input in self.input():
             with input.open('r') as in_file:
                 for line in in_file:
-                    time.sleep(1)
+                    time.sleep(0.001)
                     datas.append(line.strip())
                     
         with self.output().open('w') as out_file:
             for data in datas:
-                print >> out_file, data[0]
+                print >> out_file, data
                 
 if __name__ == '__main__':
     luigi.run(main_task_cls=Merged)
